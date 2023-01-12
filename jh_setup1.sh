dca2922cac57ed4882056a2ab996990ce3cf8b1b
Junghwan <personaljh3@gmail.com>
2023-01-12 23:11:19 +0900

    another way to install ros2

--- jh_setup1.sh
+++ jh_setup1.sh
@@ -1,17 +1,28 @@
-#지역설정
+#1. 지역설정
 sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
 
-#소스설정
-sudo apt update&&sudo apt install curl gnupg2 lsb_relased
+# 2. 소스설정
+sudo apt update&&sudo apt install curl gnupg2 lsb-release
 sudo curl -sSL https://raw.githubusercontent.com/ros/riosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gnupg2
 
+# 2. ROS 2 GPG key 
+sudo apt update && sudo apt install curl
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
-#ROS2 패키지 설치 
+# 3. source list 저장소 추가
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
+
+# 3. ROS2 패키지 설치 
 sudo apt update
 sudo apt install ros-foxy-desktop ros-foxy-rmw-fastrtps* ros-foxy-rmw-cyclonedds*
+
+#ROS 풀버전 설치
+sudo apt install ros-foxy-desktop python3-argcomplete
+
+
 
 #ROS 패키지 설치확인
 source /opt/ros/foxy/setup.bash
