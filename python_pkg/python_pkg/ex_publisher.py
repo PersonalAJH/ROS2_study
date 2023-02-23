37a45f209706c7e7b4e764a94ba13c05fd37a82d
Junghwan <personaljh3@gmail.com>
2023-02-23 23:08:59 +0900

    create ros2 cpp package publisher node

--- python_pkg/python_pkg/ex_publisher.py
+++ python_pkg/python_pkg/ex_publisher.py
@@ -1,6 +1,6 @@
 import rclpy
 from rclpy.node import Node
-from rclpy.qos import QoSProfile
+from rclpy.qos import QoSProfile        #QoS는 ROS의 퍼블리셔 설정을 위한 라이브러리
 from std_msgs.msg import String
 # from geometry_msgs.msg import Twist
 
@@ -8,12 +8,12 @@ class Example_publisher(Node):
     
     def __init__(self):
         super().__init__('ex_publisher')
-        qos_profile = QoSProfile(depth=10)
+        qos_profile = QoSProfile(depth=10)      # Publisher 의 buffer를 10개로 지정-> 혹시나 통신상태가 원활하지 못할 경우
         self.ex_publisher = self.create_publisher(String, 'helloworld', qos_profile)
         self.timer= self.create_timer(1,self.publish_helloworld_msg)
         self.count = 0
 
-    def publish_hello_msg(self):
+    def publish_helloworld_msg(self):
         msg = String()
         msg.data = 'hello world " {0}'.format(self.count)
         self.ex_publisher.publish(msg)
---
+++ rclcpp_pkg/cpp_publisher.cpp
@@ -0,0 +1,40 @@
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+// #include "geometry_msgs/msg/twist.hpp"
+
+using namespace std::chrono_literals;
+
+class cppPublisher : public rclcpp::Node
+{
+    public:
+    HelloworldPublisher() : Node("helloworld_publisher"), count(0)
+    {
+        auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+        helloworld_publisher_ = this->create_puiblisher<std_msgs::msg::String>("hellowworld",qos_profile);
+        timer_ = this->create_wall_timer(1s, std::bind(&HelloworldPublisher::publish_helloworld_msg, this));
+    }
+    private:
+    void publish_helloworld_msg(){
+        auto msg = std_msgs::msg::String();
+        msg.data = "Hello World: " + std::to_string(count++);
+        RCLCPP_INFO(this->get_logger(), "Published message : '%s'",msg,data.c_str());
+        helloworld_publisher->publish(msg);
+    }
+    rclcpp::TimerBase::SharedPtr timer_;
+    rclcpp::Publisher<std_msgs::msg::String>::SharePtr helloworld_publisher_;
+    sizae_t count_;
+};
+
+int main(int argc, char *argv[])
+{
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<HelloworldPublisher>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}
